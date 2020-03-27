d1647f258da2df8550c145362e9a63325c84d6b6
salwaelmohandes <60805227+salwaelmohandes@users.noreply.github.com>
2020-03-27 15:55:52 -0400

    project-5-public-API-requests

---
+++ README.md
@@ -0,0 +1,20 @@
+
+// **Public API requests project**
+
+I changed the body background color.
+
+I added shadow to the cards.
+I changed the cards background color.
+
+I changed the font-size, font-weight and font-family of the title.
+I added shadow to the title text and changed its color.
+
+
+// Make an Ajax single request and pull 12 random users from the API along with some basic information .
+// Display new random employee information each time user refreshes the page using the response.
+// Place search input and submit button in the header.
+// Generate the markup for each profile.
+// Create modal window to display the employee details when user click any card.
+// Switch back and forth between employees when the detail modal window is open.
+    // Create button to close the modal window.
+// Filter the employee by name with dynamically added search feature.
---
+++ js/script.js
@@ -0,0 +1,149 @@
+// **Public API requests project**
+
+const randomUrl = 'https://randomuser.me/api/?nat=us&results=12';
+const headerDiv = document.getElementsByClassName("header-inner-container");
+const searchDiv = document.getElementsByClassName("search-container");
+const galleryDiv = document.getElementsByClassName('gallery');
+
+// Make an Ajax single request and pull 12 random users from the API along with some basic information .
+// Display new random employee information each time user refreshes the page using the response.
+
+function getJSON(randomUrl, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET',randomUrl);
+    xhr.onload = () => {
+        if( xhr.status === 200) {
+            let data = JSON.parse(xhr.responseText);            
+            generateHTML(data.results.slice(0,12));
+            return callback(data);
+        }
+    }
+    xhr.send();
+}
+
+// Place search input and submit button in the header.
+headerDiv[0].appendChild(searchDiv[0])
+searchDiv[0].innerHTML=`<form action="#" method="get">
+    <input type="search" id="search-input" class="search-input" placeholder="Search...">
+    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+    </form>`
+
+// Generate the markup for each profile.
+function generateHTML(data){
+    data.map(employee => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const employeeImage= document.createElement('div');
+        const employeeItems = document.createElement('div');
+        employeeImage.setAttribute('class',"card-img-container");
+        employeeItems.setAttribute('class',"card-info-container");
+
+        galleryDiv[0].appendChild(card);
+        card.appendChild(employeeImage);
+        card.appendChild(employeeItems);
+
+        employeeImage.innerHTML=
+        `<img class="card-img" src=${employee.picture.thumbnail} alt="profile picture">`
+        employeeItems.innerHTML=
+        `<h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+        <p class="card-text">${employee.email}</p>
+        <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>`
+    })
+};
+
+let results = [];
+getJSON(randomUrl, (returnedData) => {
+    results = returnedData.results;
+});
+
+// Create modal window to display the employee details when user click any card.
+// Switch back and forth between employees when the detail modal window is open.
+
+gallery.addEventListener('click', event => {
+    const element = event.target;
+    if (element.getAttribute("class") !== null && element.getAttribute("class").includes("card")) {
+
+        const cardDiv = event.target.closest(".card");
+        const nameElement = cardDiv.querySelector("#name");
+        const firstName = nameElement.textContent.split(" ")[0];
+        const lastName = nameElement.textContent.split(" ")[1];
+
+        const employee = results.find(employee => employee.name.first === firstName 
+            && employee.name.last === lastName);
+
+        const modalDiv=document.createElement("div");
+        modalDiv.setAttribute('class',"modal-container");
+        const modal=document.createElement("div");
+        modal.setAttribute('class',"modal");
+
+    // Create button to close the modal window.
+        const closeButton=document.createElement('button');
+        closeButton.setAttribute('class', 'modal-close-btn');
+        closeButton.setAttribute('type','button');
+        closeButton.setAttribute('id', 'modal-close-btn');
+        closeButton.innerHTML=`<strong>X</strong>`
+
+        const modalInfoDiv=document.createElement('div');
+        modalInfoDiv.setAttribute('class', 'modal-info-container');
+        gallery.appendChild(modalDiv).appendChild(modal).appendChild(closeButton)
+        modal.appendChild(modalInfoDiv);
+        modalInfoDiv.innerHTML=`
+            <img class="modal-img" src=${employee.picture.thumbnail} alt="profile picture">
+            <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+            <p class="modal-text">${employee.email}</p>
+            <p class="modal-text cap">${employee.location.city}</p>
+            <hr>
+            <p class="modal-text">${employee.phone}</p>
+            <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.state}, ${employee.location.postcode}</p>
+            <p class="modal-text">Birthday: ${new Date(employee.dob.date).toLocaleDateString("en-US")}</p>`;
+
+        closeButton.addEventListener('click',(e)=>{
+            modalDiv.remove();
+        });
+    }
+});
+
+const searchButton=document.getElementById('search-submit');
+const searchInput=document.getElementById('search-input');
+
+searchButton.addEventListener('click',()=>{
+    searchForEmployee(searchInput.value.toLowerCase());
+});
+
+searchInput.addEventListener('keyup',(e)=>{
+    searchForEmployee(e.target.value.toLowerCase());
+});
+
+// Filter the employee by name with dynamically added search feature.
+function searchForEmployee(input) {
+    let errorMessage = galleryDiv.gallery.querySelector("#errorMessage");
+    if(errorMessage !== null) {
+        errorMessage.remove();
+    }
+
+    let cards = document.querySelectorAll(".card");
+    cards.forEach(card => {
+        cardElement = card.querySelector("#name");
+        card.style.display = '';
+    });
+    cards.forEach(card => {
+        cardElement = card.querySelector("#name");
+        if(!cardElement.textContent.toLowerCase().includes(input)) {
+            card.style.display = 'none';
+        }
+    });
+    
+    let hiddenCounter = 0;
+    cards.forEach(card => {
+        if(card.style.display === "none") {
+            hiddenCounter = hiddenCounter + 1;
+        }
+    })
+    if(hiddenCounter === 12) {
+        const noResults = document.createElement("h3");
+        noResults.setAttribute('id', 'errorMessage');
+        noResults.textContent = "No Results found";
+        galleryDiv.gallery.appendChild(noResults);
+    }
+};
